281be2504c786eda6789904344a00a4d11576dc2
omerozturk18 <omer_ozturk1997@hotmail.com>
2021-04-25 13:20:40 +0300

    Ders 14 Commit

---
+++ DataAccessLibrary/Abstract/ICategoryDal.cs
@@ -0,0 +1,17 @@
+﻿using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLibrary.Abstract
+{
+    public interface ICategoryDal
+    {
+        List<Category> List();
+        void Add(Category category);
+        void Update(Category category);
+        void Delete(Category category);
+    }
+}
