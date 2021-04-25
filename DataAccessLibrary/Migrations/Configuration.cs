8bc62962138ced8addd52fc8627283b72573461f
omerozturk18 <omer_ozturk1997@hotmail.com>
2021-04-25 13:04:37 +0300

    Ders 11 Commit

---
+++ DataAccessLibrary/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+﻿namespace DataAccessLibrary.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<DataAccessLibrary.Concrete.Context>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+        }
+
+        protected override void Seed(DataAccessLibrary.Concrete.Context context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
--- EntityLayer/Concrete/Heading.cs
+++ EntityLayer/Concrete/Heading.cs
@@ -19,7 +19,7 @@ namespace EntityLayer.Concrete
         public int CategoryId { get; set; }
         public virtual Category Category { get; set; }
 
-        public int WriterId { get; set; }
+        public int? WriterId { get; set; }
         public virtual Writer Writer { get; set; }
 
         public ICollection<Content> Contents { get; set; }
