58938acc69c7b1a3633d5a55eb8396258512c056
omerozturk18 <omer_ozturk1997@hotmail.com>
2021-04-25 13:48:03 +0300

    Ders 18 Commit

---
+++ DataAccessLibrary/Concrete/Repositories/GenericRepository.cs
@@ -0,0 +1,50 @@
+﻿using DataAccessLibrary.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLibrary.Concrete.Repositories
+{
+    public class GenericRepository<T> : IRepository<T> where T : class
+    {
+        Context _context;
+        DbSet<T> _object;
+
+        public GenericRepository(Context context)
+        {
+            _context = context;
+            _object = _context.Set<T>();
+        }
+
+        public void Add(T entity)
+        {
+            _object.Add(entity);
+            _context.SaveChanges();
+        }
+
+        public void Delete(T entity)
+        {
+            _object.Remove(entity);
+            _context.SaveChanges();
+        }
+
+        public List<T> GetAll()
+        {
+            return _object.ToList();
+        }
+
+        public List<T> List(Expression<Func<T, bool>> filter)
+        {
+            return _object.Where(filter).ToList();
+        }
+
+        public void Update(T entity)
+        {
+            _context.SaveChanges();
+        }
+    }
+}
